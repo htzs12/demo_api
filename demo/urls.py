679b893576aa80e8b6d2e677a6bdaac924fe5b4f
htzs12 <www.htzs@qq.com>
2019-03-06 11:21:23 +0800

    update-2

--- demo/urls.py
+++ demo/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import MapView
+from .views import MapView,UsersView
 
 
 app_name = 'demo'
 
 urlpatterns = [
     path('',MapView.as_view(),name='map'),
+    path('users/',UsersView.as_view(),name='user')
 ]
--- demo/views.py
+++ demo/views.py
@@ -2,8 +2,26 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Map
 from .serializers import MapSerializers
+from django.views.generic.base import View
+from django.http import HttpResponse
 
 
 class MapView(viewsets.ModelViewSet):
     queryset = Map.objects.all()
     serializer_class = MapSerializers
+
+"""
+返回token
+"""
+class UsersView(View):
+    def get(self,request):
+        return HttpResponse('ok')
+
+    def post(self,request):
+        account = request.POST.get('account')
+        passwd = request.POST.get('passwd')
+
+        print(account)
+        print(passwd)
+
+        return HttpResponse('haha')
