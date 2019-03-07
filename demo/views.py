6911aeb8fc42dbb489ceb57e4c126c3a63aa44a6
htzs12 <www.htzs@qq.com>
2019-03-07 09:08:40 +0800

    update-22

--- demo/views.py
+++ demo/views.py
@@ -23,6 +23,7 @@ class MapView(viewsets.ModelViewSet):
 
 def user(request):
     if request.method == 'POST':
+        data = {}
         account = request.POST.get('account')
         passwd = request.POST.get('passwd')
         if account == 'haoge' and passwd == '123456':
